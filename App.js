37e76ccfacd5110ca53e4f98a1223af12aa422bd
bhavyashah9873 <bhavyashah9873@gmail.com>
2022-12-25 03:01:09 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+import Home from './src/screens/Home';
+
+const App = () => {
+  return <Home />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import {SafeAreaView, View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
